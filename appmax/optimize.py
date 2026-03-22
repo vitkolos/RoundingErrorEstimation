1806952928160b2d35f7a6655055b0226e7b2b7b
Vít Kološ <vit.kolos@gmail.com>
2026-03-22 23:51:59 +0100

    Check feasibility

--- appmax/optimize.py
+++ appmax/optimize.py
@@ -1,4 +1,5 @@
 import torch
+import numpy as np
 import scipy.optimize
 import appmax.neurons
 import appmax.evaluation
@@ -10,11 +11,13 @@ def find_appmax(eval_net: appmax.evaluation.EvaluationNet, sample: torch.Tensor,
     constraints = appmax.neurons.Constraints()
     message = appmax.neurons.Message(sample)
     message = appmax.neurons.collect(eval_net, message, constraints)
-    sample_found, err_found = optimize(message, constraints, bounds=eval_net.bounds, verbose=verbose)
+    c, bias, A_ub, b_ub = prepare_lp(message, constraints)
+    check_feasibility(sample, A_ub, b_ub, eval_net.bounds)
+    sample_found, err_found = optimize(c, bias, A_ub, b_ub, eval_net.bounds, verbose)
     return sample_found.reshape_as(sample), err_found
 
 
-def optimize(message: appmax.neurons.Message, constraints: appmax.neurons.Constraints, bounds: Bounds, verbose: bool) -> tuple[torch.Tensor, float]:
+def prepare_lp(message: appmax.neurons.Message, constraints: appmax.neurons.Constraints) -> tuple[torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor]:
     TOL = 0  # 1e-8
 
     # objective to minimize
@@ -30,10 +33,15 @@ def optimize(message: appmax.neurons.Message, constraints: appmax.neurons.Constr
     S_weight = torch.cat(constraints.S_weight)
     S_bias = -torch.cat(constraints.S_bias) + TOL
 
-    c = objective.squeeze().cpu().numpy()
-    A_ub = torch.cat((U_weight, S_weight)).cpu().numpy()
-    b_ub = torch.cat((U_bias, S_bias)).cpu().numpy()
-    result = scipy.optimize.linprog(c, A_ub, b_ub, bounds=bounds, options={'disp': verbose})
+    c = objective.squeeze()
+    bias = message.s_bias
+    A_ub = torch.cat((U_weight, S_weight))
+    b_ub = torch.cat((U_bias, S_bias))
+    return c, bias, A_ub, b_ub
+
+
+def optimize(c: torch.Tensor, bias: torch.Tensor, A_ub: torch.Tensor, b_ub: torch.Tensor, bounds: Bounds, verbose: bool) -> tuple[torch.Tensor, float]:
+    result = scipy.optimize.linprog(c.numpy(), A_ub.numpy(), b_ub.numpy(), bounds=bounds, options={'disp': verbose})
 
     if not result.success:
         match result.status:
@@ -47,5 +55,33 @@ def optimize(message: appmax.neurons.Message, constraints: appmax.neurons.Constr
         raise RuntimeError('result is empty')
 
     found_x = torch.from_numpy(result.x).to(dtype=torch.get_default_dtype())
-    found_maximum = -result.fun + message.s_bias.item()
+    found_maximum = -result.fun + bias.item()
     return found_x, found_maximum
+
+
+def check_feasibility(sample: torch.Tensor, A_ub: torch.Tensor, b_ub: torch.Tensor, bounds: Bounds):
+    infeasible = False
+    sample_flat = sample.flatten()
+
+    bounds_tensor = torch.atleast_2d(torch.tensor(np.array(bounds, dtype=float)))
+    too_low = torch.nonzero(sample_flat < bounds_tensor[:, 0]).flatten().tolist()
+    too_high = torch.nonzero(sample_flat > bounds_tensor[:, 1]).flatten().tolist()
+
+    if too_low:
+        infeasible = True
+        print(f'indices {too_low} < lower bounds')
+
+    if too_high:
+        infeasible = True
+        print(f'indices {too_high} > upper bounds')
+
+    infeasible_rows = torch.nonzero(A_ub @ sample_flat > b_ub).flatten()
+
+    if len(infeasible_rows) > 0:
+        infeasible = True
+
+        for i in infeasible_rows:
+            print('infeasible constraint', A_ub[i], '@ x <=', b_ub[i])
+
+    if infeasible:
+        raise RuntimeError(f'infeasible (check the output above); input tensor:\n{sample}')
