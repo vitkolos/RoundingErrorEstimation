576e3eede3dcd487f5155becf7f9e8beba24a9e2
Vít Kološ <vit.kolos@gmail.com>
2026-07-21 00:52:00 +0200

    Further simplify intervals

--- replication/propagate_intervals/intervals_simplified.py
+++ replication/propagate_intervals/intervals_simplified.py
@@ -6,53 +6,74 @@ import torch.nn.functional as F
 from network import SmallDenseNet
 from dataset import create_dataset
 
-ZERO = torch.tensor(0.0).double()
+
+def n_relu(w):
+    return -F.relu(-w)
 
 
-def negative(w):
-    return torch.minimum(ZERO, w)
+class Message:
+    def __init__(self, shape):
+        self.a = torch.full(shape, torch.inf).double()
+        self.b = torch.full(shape, -torch.inf).double()
+        self.b_old = torch.full(shape, -torch.inf).double()
+        self.alpha = torch.zeros(shape).double()
+        self.beta = torch.zeros(shape).double()
+        self.raw_input = True
+
+    def report(self, layer):
+        assert torch.all(self.a <= self.b)
+        alpha, beta = self.alpha.flatten(), self.beta.flatten()
+        assert torch.all(alpha <= 0)
+        assert torch.all(beta >= 0)
+
+        diff = beta-alpha
+        imin, imax = torch.argmin(diff), torch.argmax(diff)
+        print(f'layer {layer}:', f'min [{alpha[imin].item():.4f}, {beta[imin].item():.4f}]',
+              f'max [{alpha[imax].item():.4f}, {beta[imax].item():.4f}]')
 
 
-def positive(w):
-    return torch.maximum(ZERO, w)
-
-
-def compute_input_ai_bi(class_=None):
+def compute_input_ab(class_=None):
     test_data = create_dataset(train=False, batch_size=BATCH_SIZE)
-    a = torch.full((INPUT_LENGTH,), torch.inf).double()
-    b = torch.full((INPUT_LENGTH,), -torch.inf).double()
+    message = Message(INPUT_SHAPE)
 
     for xs, ys in tqdm.tqdm(test_data):
         if class_ is not None:
             xs = xs[ys == class_]
 
         if len(xs) > 0:
-            xs = xs.double().flatten(1)
+            xs = xs.double()
             a_batch, _ = torch.min(xs, dim=0)
             b_batch, _ = torch.max(xs, dim=0)
-            a = torch.minimum(a, a_batch)
-            b = torch.maximum(b, b_batch)
+            message.a = torch.minimum(message.a, a_batch)
+            message.b = torch.maximum(message.b, b_batch)
 
-    return a, b
+    return message
 
 
-def compute_output_ai_bi(a, b, w, bias, relu=True):
-    w_neg = negative(w)
-    w_pos = positive(w)
+def compute_layer_ab(message: Message, w, bias):
+    w_neg = n_relu(w)
+    w_pos = F.relu(w)
 
-    a_new = bias + torch.matmul(w_neg, b) + torch.matmul(w_pos, a)
-    b_new = bias + torch.matmul(w_neg, a) + torch.matmul(w_pos, b)
+    a_new = bias + message.b @ w_neg.T + message.a @ w_pos.T
+    b_new = bias + message.a @ w_neg.T + message.b @ w_pos.T
 
-    if relu:
-        return F.relu(a_new), F.relu(b_new)
+    message.b_old = message.b
+    message.a, message.b = a_new, b_new
+    return message
+
+
+def shift_bias(message: Message, w, bias):
+    if message.raw_input:
+        bias = bias + message.a @ w.T
+        message.b -= message.a
+        message.a -= message.a
+        message.raw_input = False
+        assert torch.all(message.b >= 0)
     else:
-        return a_new, b_new
+        assert torch.all(message.b > 0)
 
-
-def compute_bias_shift(a, b, w, bias):
-    bias = bias + torch.matmul(w, a)
-    a, b = a-a, b-a
-    return bias, a, b
+    assert torch.all(message.a == 0)
+    return message, bias
 
 
 def load_network():
@@ -62,67 +83,34 @@ def load_network():
     return net
 
 
-def get_layer(net, layer):
-    x = torch.zeros(INPUT_SHAPE).unsqueeze(0).double()
-    input_size = INPUT_SHAPE
-    layer_pointer = None
-
-    for child in net.children():
-        assert isinstance(child, nn.Sequential)
-        i = 0  # count only conv, fc and pool
-
-        for l in child.children():
-            x = l(x)
-
-            if isinstance(l, nn.Dropout) or isinstance(l, nn.Flatten) or isinstance(l, nn.ReLU):
-                pass
-            elif i < layer:
-                input_size = x.shape
-                i += 1
-            else:
-                layer_pointer = l
-                break
-
-    if type(layer_pointer) == nn.Linear:
-        return "lin", layer_pointer.weight, layer_pointer.bias, input_size, layer_pointer
-
-    if type(layer_pointer) == nn.MaxPool2d:
-        return "pool", None, None, input_size, layer_pointer
-
-    raise ValueError(f"{layer} is wierd")
-
-
-def delta(x):
-    return hat(x) - x
-
-
 def hat(x):
     return x.half().double()
 
 
-def compute_output_alpha_beta(alpha, beta, b, delta_weight, delta_bias, weight, bias, relu=True):
+def compute_layer_interval(message: Message, weight, bias):
     weight_hat = hat(weight)
     bias_hat = hat(bias)
+    delta_weight = weight_hat - weight
+    delta_bias = bias_hat - bias
 
-    delta_neg = negative(delta_weight)
-    delta_pos = positive(delta_weight)
+    delta_neg = n_relu(delta_weight)
+    delta_pos = F.relu(delta_weight)
 
-    alpha_new = torch.matmul(delta_neg, b)
-    beta_new = torch.matmul(delta_pos, b)
+    alpha_new = message.b_old @ delta_neg.T
+    beta_new = message.b_old @ delta_pos.T
 
-    weight_hat_neg = negative(weight_hat)
-    weight_hat_pos = positive(weight_hat)
+    weight_hat_neg = n_relu(weight_hat)
+    weight_hat_pos = F.relu(weight_hat)
 
-    alpha_new += torch.matmul(weight_hat_pos, alpha)
-    alpha_new += torch.matmul(weight_hat_neg, beta)
+    alpha_new += message.alpha @ weight_hat_pos.T
+    alpha_new += message.beta @ weight_hat_neg.T
 
-    beta_new += torch.matmul(weight_hat_neg, alpha)
-    beta_new += torch.matmul(weight_hat_pos, beta)
+    beta_new += message.alpha @ weight_hat_neg.T
+    beta_new += message.beta @ weight_hat_pos.T
 
-    if relu:
-        return (negative(alpha_new + delta_bias), F.relu(beta_new + delta_bias))
-    else:
-        return alpha_new, beta_new
+    message.alpha = alpha_new + delta_bias
+    message.beta = beta_new + delta_bias
+    return message
 
 
 def compute_pool_alpha_beta(alpha, beta, shape, max_):
@@ -131,42 +119,43 @@ def compute_pool_alpha_beta(alpha, beta, shape, max_):
     return -1*max_(-1*alpha_new).flatten(), max_(beta_new).flatten()
 
 
-def compute_pool_ai_bi(a, b, shape, max_):
+def compute_pool_ab(a, b, shape, max_):
     a_new = a.reshape(*shape)
     b_new = b.reshape(*shape)
     return max_(a_new).flatten(), max_(b_new).flatten()
 
 
-def calculate_output_intervals(class_):
+def calculate_intervals(class_):
     with torch.no_grad():
-        a, b = compute_input_ai_bi(class_)
-        alpha = torch.zeros(INPUT_LENGTH).double()
-        beta = torch.zeros(INPUT_LENGTH).double()
+        message = compute_input_ab(class_)
         net = load_network()
 
-        for layer in range(LAYERS):
-            type_, weight, bias, input_shape, layer_pointer = get_layer(net, layer)
+        # if type_ == "pool":
+        #     alpha, beta = compute_pool_alpha_beta(alpha, beta, input_shape, layer_pointer)
+        #     a, b = compute_pool_ab(a, b, input_shape, layer_pointer)
 
-            if type_ == "pool":
-                alpha, beta = compute_pool_alpha_beta(alpha, beta, input_shape, layer_pointer)
-                a, b = compute_pool_ai_bi(a, b, input_shape, layer_pointer)
-            else:
-                bias, a, b = compute_bias_shift(a, b, weight, bias)
-                delta_weight, delta_bias = delta(weight), delta(bias)
-                alpha, beta = compute_output_alpha_beta(
-                    alpha, beta, b, delta_weight, delta_bias, weight, bias, relu=(layer != (LAYERS-1)))
-                a, b = compute_output_ai_bi(a, b, weight, bias, relu=layer != (LAYERS-1))
-                del weight
-                del bias
+        for i, layer in enumerate(net.network):
+            match layer:
+                case nn.Dropout():
+                    pass
+                case nn.Flatten():
+                    message.a = layer(message.a)
+                    message.b = layer(message.b)
+                    message.alpha = layer(message.alpha)
+                    message.beta = layer(message.beta)
+                case nn.Linear():
+                    bias = layer.bias if layer.bias is not None else torch.zeros(layer.weight.shape[0]).double()
+                    message, bias = shift_bias(message, layer.weight, bias)
+                    message = compute_layer_ab(message, layer.weight, bias)
+                    message = compute_layer_interval(message, layer.weight, bias)
+                case nn.ReLU():
+                    message.a = F.relu(message.a)
+                    message.b = F.relu(message.b)
+                    message.alpha = n_relu(message.alpha)
+                    message.beta = F.relu(message.beta)
+                    message.report(i)
 
-            assert torch.all(a <= b)
-            assert torch.all(alpha <= 0)
-            assert torch.all(beta >= 0)
-
-            diff = beta-alpha
-            imin, imax = torch.argmin(diff), torch.argmax(diff)
-            print(f'layer {layer+1}:', f'min [{alpha[imin].item():.4f}, {beta[imin].item():.4f}]',
-                  f'max [{alpha[imax].item():.4f}, {beta[imax].item():.4f}]')
+        message.report(i)
 
 
 if __name__ == "__main__":
@@ -177,4 +166,4 @@ if __name__ == "__main__":
     INPUT_SHAPE = (1, 28, 28)
     INPUT_LENGTH = 1 * 28 * 28
 
-    calculate_output_intervals(class_=0)
+    calculate_intervals(class_=0)
