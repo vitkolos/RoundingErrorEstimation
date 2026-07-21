f77c4799c6e41b0292e7aabc52b9cfd45fbc95cb
Vít Kološ <vit.kolos@gmail.com>
2026-07-21 18:09:32 +0200

    Add convolution to intervals

--- replication/propagate_intervals/intervals_simplified.py
+++ replication/propagate_intervals/intervals_simplified.py
@@ -3,12 +3,19 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-from network import SmallDenseNet
+from network import SmallDenseNet, SmallConvNet
 from dataset import create_dataset
 
 
 def n_relu(w):
     return -F.relu(-w)
+
+
+def load_network():
+    net = MODEL()
+    net.load_state_dict(torch.load(NETWORK, map_location='cpu'))
+    net.eval().double()
+    return net
 
 
 class Message:
@@ -18,7 +25,14 @@ class Message:
         self.b_old = torch.full(shape, -torch.inf).double()
         self.alpha = torch.zeros(shape).double()
         self.beta = torch.zeros(shape).double()
-        self.raw_input = True
+        self.input_processed = False
+
+    def apply_special(self, module: nn.Module):
+        self.a = module(self.a)
+        self.b = module(self.b)
+        self.alpha = -module(-self.alpha)
+        self.beta = module(self.beta)
+        return self
 
     def report(self, layer):
         assert torch.all(self.a <= self.b)
@@ -32,9 +46,9 @@ class Message:
               f'max [{alpha[imax].item():.4f}, {beta[imax].item():.4f}]')
 
 
-def compute_input_ab(class_=None):
+def input_ab_message(class_=None):
     test_data = create_dataset(train=False, batch_size=BATCH_SIZE)
-    message = Message(INPUT_SHAPE)
+    message = Message(shape=(1, *test_data.dataset[0][0].shape))
 
     for xs, ys in tqdm.tqdm(test_data):
         if class_ is not None:
@@ -50,110 +64,93 @@ def compute_input_ab(class_=None):
     return message
 
 
-def compute_layer_ab(message: Message, w, bias):
-    w_neg = n_relu(w)
-    w_pos = F.relu(w)
+def shift_bias(layer: nn.Module, message: Message):
+    """accounts for the fact that input values can be below zero (using this new bias we can act as if they are not)"""
+    if not message.input_processed:
+        bias = layer(message.a)
+        message.b -= message.a
+        message.a -= message.a
+        message.input_processed = True
+        assert torch.all(message.b >= 0)
+    else:
+        bias = layer.bias if layer.bias is not None else torch.zeros(layer.weight.shape[0]).double()
 
-    a_new = bias + message.b @ w_neg.T + message.a @ w_pos.T
-    b_new = bias + message.a @ w_neg.T + message.b @ w_pos.T
+        if isinstance(layer, nn.Conv2d):
+            bias = bias[..., None, None]
+
+        # assert torch.all(message.b > 0)
+
+    # assert torch.all(message.a == 0)
+    return message, bias
+
+
+def layer_ab(layer: nn.Module, message: Message, bias: torch.Tensor):
+    w_neg = n_relu(layer.weight)
+    w_pos = F.relu(layer.weight)
+
+    match layer:
+        case nn.Linear():
+            a_new = bias + F.linear(message.b, w_neg) + F.linear(message.a, w_pos)
+            b_new = bias + F.linear(message.a, w_neg) + F.linear(message.b, w_pos)
+        case nn.Conv2d():
+            def conv(x, w): return layer._conv_forward(x, w, None)
+            a_new = bias + conv(message.b, w_neg) + conv(message.a, w_pos)
+            b_new = bias + conv(message.a, w_neg) + conv(message.b, w_pos)
 
     message.b_old = message.b
     message.a, message.b = a_new, b_new
     return message
 
 
-def shift_bias(message: Message, w, bias):
-    if message.raw_input:
-        bias = bias + message.a @ w.T
-        message.b -= message.a
-        message.a -= message.a
-        message.raw_input = False
-        assert torch.all(message.b >= 0)
-    else:
-        assert torch.all(message.b > 0)
-
-    assert torch.all(message.a == 0)
-    return message, bias
-
-
-def load_network():
-    net = MODEL()
-    net.load_state_dict(torch.load(NETWORK, map_location='cpu'))
-    net.eval().double()
-    return net
-
-
-def hat(x):
+def tilde(x):
     return x.half().double()
 
 
-def compute_layer_interval(message: Message, weight, bias):
-    weight_hat = hat(weight)
-    bias_hat = hat(bias)
-    delta_weight = weight_hat - weight
-    delta_bias = bias_hat - bias
+def layer_alpha_beta(layer: nn.Module, message: Message, bias: torch.Tensor):
+    weight_tilde = tilde(layer.weight)
+    bias_tilde = tilde(bias)
+    weight_delta = weight_tilde - layer.weight
+    bias_delta = bias_tilde - bias
+    weight_delta_neg = n_relu(weight_delta)
+    weight_delta_pos = F.relu(weight_delta)
+    weight_tilde_neg = n_relu(weight_tilde)
+    weight_tilde_pos = F.relu(weight_tilde)
 
-    delta_neg = n_relu(delta_weight)
-    delta_pos = F.relu(delta_weight)
+    match layer:
+        case nn.Linear():
+            alpha_new = bias_delta + F.linear(message.b_old, weight_delta_neg)
+            beta_new = bias_delta + F.linear(message.b_old, weight_delta_pos)
+            alpha_new += F.linear(message.alpha, weight_tilde_pos) + F.linear(message.beta, weight_tilde_neg)
+            beta_new += F.linear(message.alpha, weight_tilde_neg) + F.linear(message.beta, weight_tilde_pos)
+        case nn.Conv2d():
+            def conv(x, w): return layer._conv_forward(x, w, None)
+            alpha_new = bias_delta + conv(message.b_old, weight_delta_neg)
+            beta_new = bias_delta + conv(message.b_old, weight_delta_pos)
+            alpha_new += conv(message.alpha, weight_tilde_pos) + conv(message.beta, weight_tilde_neg)
+            beta_new += conv(message.alpha, weight_tilde_neg) + conv(message.beta, weight_tilde_pos)
 
-    alpha_new = message.b_old @ delta_neg.T
-    beta_new = message.b_old @ delta_pos.T
-
-    weight_hat_neg = n_relu(weight_hat)
-    weight_hat_pos = F.relu(weight_hat)
-
-    alpha_new += message.alpha @ weight_hat_pos.T
-    alpha_new += message.beta @ weight_hat_neg.T
-
-    beta_new += message.alpha @ weight_hat_neg.T
-    beta_new += message.beta @ weight_hat_pos.T
-
-    message.alpha = alpha_new + delta_bias
-    message.beta = beta_new + delta_bias
+    message.alpha, message.beta = alpha_new, beta_new
     return message
 
 
-def compute_pool_alpha_beta(alpha, beta, shape, max_):
-    alpha_new = alpha.reshape(*shape)
-    beta_new = beta.reshape(*shape)
-    return -1*max_(-1*alpha_new).flatten(), max_(beta_new).flatten()
-
-
-def compute_pool_ab(a, b, shape, max_):
-    a_new = a.reshape(*shape)
-    b_new = b.reshape(*shape)
-    return max_(a_new).flatten(), max_(b_new).flatten()
-
-
-def calculate_intervals(class_):
+def intervals(class_):
     with torch.no_grad():
-        message = compute_input_ab(class_)
+        message = input_ab_message(class_)
         net = load_network()
-
-        # if type_ == "pool":
-        #     alpha, beta = compute_pool_alpha_beta(alpha, beta, input_shape, layer_pointer)
-        #     a, b = compute_pool_ab(a, b, input_shape, layer_pointer)
 
         for i, layer in enumerate(net.network):
             match layer:
                 case nn.Dropout():
                     pass
                 case nn.Flatten():
-                    message.a = layer(message.a)
-                    message.b = layer(message.b)
-                    message.alpha = layer(message.alpha)
-                    message.beta = layer(message.beta)
-                case nn.Linear():
-                    bias = layer.bias if layer.bias is not None else torch.zeros(layer.weight.shape[0]).double()
-                    message, bias = shift_bias(message, layer.weight, bias)
-                    message = compute_layer_ab(message, layer.weight, bias)
-                    message = compute_layer_interval(message, layer.weight, bias)
-                case nn.ReLU():
-                    message.a = F.relu(message.a)
-                    message.b = F.relu(message.b)
-                    message.alpha = n_relu(message.alpha)
-                    message.beta = F.relu(message.beta)
+                    message = message.apply_special(layer)
+                case nn.ReLU() | nn.MaxPool2d():
+                    message = message.apply_special(layer)
                     message.report(i)
+                case nn.Linear() | nn.Conv2d():
+                    message, bias = shift_bias(layer, message)
+                    message = layer_ab(layer, message, bias)
+                    message = layer_alpha_beta(layer, message, bias)
 
         message.report(i)
 
@@ -162,8 +159,7 @@ if __name__ == "__main__":
     BATCH_SIZE = 256
     NETWORK = "models/mnist_dense_net.pt"
     MODEL = SmallDenseNet
-    LAYERS = 3
-    INPUT_SHAPE = (1, 28, 28)
-    INPUT_LENGTH = 1 * 28 * 28
+    # NETWORK = "models/mnist_conv_net.pt"
+    # MODEL = SmallConvNet
 
-    calculate_intervals(class_=0)
+    intervals(class_=0)
